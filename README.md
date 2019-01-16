df106b38f46df571ef4d0d366d8cc2c6513f24b1
rickt96 <riccardo.tassinari96@gmail.com>
2019-01-16 21:29:26 +0100

    css editing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # landing page test for OFTASSINARI
 
 my first test on bootstrap template for a landing page example
+
+https://www.free-css.com/free-css-templates/page234/interact
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,13 +12,18 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+
+
 // Create the email and send the message
-$to = 'onoranzetassinari@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "Messaggio ricevuto dal form del tuo sito.\n\n"."Dettagli:\n\nNome: $name\n\nEmail: $email_address\n\nMessaggio:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+$to = 'riccardo.tassinari96@gmail.com';
+
+$subject = "Messaggio ricevuto dal sito:  $name";
+
+$body = "Messaggio ricevuto dal form del tuo sito.\n".
+        "Nome: $name\n".
+        "Email: $email_address\n". 
+        "Messaggio:\n$message";
+
+mail($to,$subject,$body);
 return true;			
 ?>
